d6caa2933c3fb54b44eb20612c0f140012b73536
Mike Harris <MikeMKH@users.noreply.github.com>
2026-03-17 05:35:22 -0500

    add examples from chapter 1 pointers size and types

--- tests/chapter01/test_examples.c
+++ tests/chapter01/test_examples.c
@@ -28,3 +28,19 @@ Test(examples, const_static_pointer_init_null) {
   const int *pi = NULL;
   cr_assert_eq(pi, NULL);
 }
+
+Test(examples, size_t_given_negative_value) {
+  size_t x = -5;
+  cr_assert_eq(x, (size_t)-5);
+  
+  char buffer[24];
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wformat"
+  snprintf(buffer, sizeof(buffer), "%d", x);
+  #pragma GCC diagnostic pop
+  cr_assert_str_eq(buffer, "-5");
+  
+  snprintf(buffer, sizeof(buffer), "%zu", x);
+  cr_assert_str_eq(buffer, "18446744073709551611");
+  
+}
