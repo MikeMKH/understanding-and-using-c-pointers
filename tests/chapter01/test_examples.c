5ca5fb4c3e0db0aba2c16ffd9fabd5eba42c32c5
Mike Harris <MikeMKH@users.noreply.github.com>
2026-03-19 05:56:43 -0500

    add examples from chapter 1 common usages of pointers

--- tests/chapter01/test_examples.c
+++ tests/chapter01/test_examples.c
@@ -98,3 +98,96 @@ Test(examples, pointer_comparison) {
   cr_assert(p1 >= p0);
   cr_assert(p2 != p1);
 }
+
+Test(examples, multiple_levels_of_indirection) {
+  char *titles[] = {
+    "A Tale of Two Cities",
+    "Wuthering Heights",
+    "Don Quixote",
+    "Odyssey",
+    "Moby-Dick",
+    "Hamlet",
+    "Gulliver's Travels"
+  };
+  
+  char **bestBooks[3];
+  bestBooks[0] = &titles[0];
+  bestBooks[1] = &titles[3];
+  bestBooks[2] = &titles[5];
+  
+  char **englishBooks[4];
+  englishBooks[0] = &titles[0];
+  englishBooks[1] = &titles[1];
+  englishBooks[2] = &titles[5];
+  englishBooks[3] = &titles[6];
+  
+  cr_assert_str_eq(titles[0], "A Tale of Two Cities");
+  cr_assert_str_eq(*bestBooks[0], "A Tale of Two Cities");
+  cr_assert_str_eq(*englishBooks[0], "A Tale of Two Cities");
+  cr_assert_eq(titles, bestBooks[0]);
+  cr_assert_eq(englishBooks[0], bestBooks[0]);
+  
+  cr_assert_str_eq(titles[1], "Wuthering Heights");
+  cr_assert_str_eq(*bestBooks[1], "Odyssey");
+  cr_assert_str_eq(*englishBooks[1], "Wuthering Heights");
+}
+
+Test(examples, const_pointer_to_const) {
+  int num = 5;
+  const int limit = 100;
+  int *pi;
+  const int *pci;
+  
+  pi = &num;
+  pci = &limit;
+  
+  cr_assert_eq(*pi, 5);
+  cr_assert_eq(*pci, 100);
+  
+  pci = &num;
+  cr_assert_eq(*pci, 5);
+  /* *pci = 200; */ /* error: assignment of read-only location */
+}
+
+Test(examples, const_pointer_to_non_constant) {
+  int num;
+  const int limit = 100;
+  int *const cpi = &num;
+  
+  cr_assert_eq(*cpi, num);
+  
+  *cpi = limit;
+  cr_assert_eq(*cpi, 100);
+  
+  *cpi = 25;
+  cr_assert_eq(*cpi, 25);
+  
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
+  int *const warn = &limit; /* discards qualifiers */
+  #pragma GCC diagnostic pop
+  cr_assert_eq(*warn, 100);
+  
+  /* cpi = &num; */ /* error: cannot assign to variable 'cpi' with const-qualified type 'int *const' */
+}
+
+Test(examples, constant_pointer_to_constant) {
+  const int limit = 500;
+  const int * const cpci = &limit;
+  
+  cr_assert_eq(*cpci, 500);
+  cr_assert_eq(*cpci, limit);
+  cr_assert_eq(cpci, &limit);
+  
+  const int * const *pcpci = &cpci;
+  cr_assert_eq(**pcpci, 500);
+  cr_assert_eq(*pcpci, cpci);
+  /* *cpci = 1000; */ /* error: assignment of read-only location */
+  
+  int num = 25;
+  const int * const p = &num;
+  pcpci = &p;
+  cr_assert_eq(**pcpci, 25);
+  pcpci = &cpci;
+  cr_assert_eq(**pcpci, 500);
+}
