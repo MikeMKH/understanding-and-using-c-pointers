2d905b975fc5262549c939dce08cea50c30f6299
Mike Harris <MikeMKH@users.noreply.github.com>
2026-04-24 05:47:57 -0500

    add examples from chapter 6 from introduction

--- tests/chapter06/test_examples6.c
+++ tests/chapter06/test_examples6.c
@@ -1,9 +1,71 @@
 #include <criterion/criterion.h>
 #include <stdlib.h>
 
-Test(examples, pointer_to_string_literal) {
-  const char *s = "Hello World!";
-  cr_assert_str_eq(s, "Hello World!");
-  cr_assert_eq(sizeof(s), sizeof(char *));
-  cr_assert_eq(strlen(s), 12);
+/*
+struct _person {
+  char* firstName;
+  char* lastName;
+  char* title;
+  unsigned short age;
+};
+*/
+
+typedef struct _person {
+  char* firstName;
+  char* lastName;
+  char* title;
+  unsigned int age;
+} Person;
+
+Test(examples, person_allocated_on_stack) {
+  Person p = {"John", "Doe", "Mr.", 30};
+  
+  cr_assert_str_eq(p.firstName, "John");
+  cr_assert_str_eq(p.lastName, "Doe");
+  cr_assert_str_eq(p.title, "Mr.");
+  cr_assert_eq(p.age, 30);
+}
+
+Test(examples, person_allocated_on_stack_without_using_typedef) {
+  struct _person p = {"John", "Doe", "Mr.", 30};
+  
+  cr_assert_str_eq(p.firstName, "John");
+  cr_assert_str_eq(p.lastName, "Doe");
+  cr_assert_str_eq(p.title, "Mr.");
+  cr_assert_eq(p.age, 30);
+}
+
+Test(examples, person_allocated_on_heap) {
+  Person *p = malloc(sizeof(Person));
+  p->firstName = "John";
+  p->lastName = "Doe";
+  p->title = "Mr.";
+  p->age = 30;
+  
+  cr_assert_str_eq(p->firstName, "John");
+  cr_assert_str_eq(p->lastName, "Doe");
+  cr_assert_str_eq(p->title, "Mr.");
+  cr_assert_eq(p->age, 30);
+  
+  free(p);
+}
+
+typedef struct _person_using_short {
+  char* firstName;
+  char* lastName;
+  char* title;
+  unsigned short age;
+} PersonShort;
+
+Test(examples, person_using_short) {
+  PersonShort p = {"John", "Doe", "Mr.", 30};
+  
+  cr_assert_str_eq(p.firstName, "John");
+  cr_assert_str_eq(p.lastName, "Doe");
+  cr_assert_str_eq(p.title, "Mr.");
+  cr_assert_eq(p.age, 30);
+}
+
+Test(examples, short_and_int_take_same_amount_of_space_due_to_padding) {
+  cr_expect_eq(sizeof(Person), sizeof(PersonShort), "due to padding, both structs have the same size");
 }
