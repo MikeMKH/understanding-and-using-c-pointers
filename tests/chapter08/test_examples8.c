da7bba47c12e02b0262916f8948a647b78a863a0
Mike Harris <MikeMKH@users.noreply.github.com>
2026-05-11 05:43:41 -0500

    add examples from chapter 8 from casting pointers

--- tests/chapter08/test_examples8.c
+++ tests/chapter08/test_examples8.c
@@ -1,9 +1,31 @@
 #include <criterion/criterion.h>
 #include <stdlib.h>
 
-Test(examples, addition) {
-  int a = 2;
-  int b = 3;
-  int sum = a + b;
-  cr_assert_eq(sum, 5, "Expected %d but got %d", 5, sum);
+Test(examples, casting_an_int_to_a_pointer_to_an_int) {
+  int value = 8;
+  int *pi = &value;
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
+  int tmp = (int)pi;
+  #pragma GCC diagnostic pop
+
+  /* pi = (int*)tmp; */ /* ERROR: AddressSanitizer: SEGV on unknown address 0x00000d398500 */
+  cr_assert_eq(tmp, tmp);
+  cr_assert_eq(*pi, value);
+}
+
+Test(examples, determining_endianness_of_machine) {
+  int value = 0x12345678;
+  char *pc = (char*)&value;
+  
+  if (*pc == 0x78) {
+    cr_expect(true, "machine is little-endian");
+  } else if (*pc == 0x12) {
+    cr_expect(true, "machine is big-endian");
+  } else {
+    cr_expect_fail("unexpected endianness detected");
+  }
+  
+  cr_expect_eq(*pc, 0x78,
+    "my Mac is little-endian, I find little-endian strange but it is more common than big-endian");
 }
