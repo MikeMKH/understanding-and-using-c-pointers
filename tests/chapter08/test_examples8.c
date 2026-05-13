781c9d50b78e4a3b0e06c681915b2519022d8906
Mike Harris <MikeMKH@users.noreply.github.com>
2026-05-13 06:12:43 -0500

    add examples from chapter 8 from threads and pointers

--- tests/chapter08/test_examples8.c
+++ tests/chapter08/test_examples8.c
@@ -1,5 +1,8 @@
 #include <criterion/criterion.h>
 #include <stdlib.h>
+#include <sys/_pthread/_pthread_mutex_t.h>
+#include <sys/_pthread/_pthread_t.h>
+#include <pthread/pthread.h>
 
 Test(examples, casting_an_int_to_a_pointer_to_an_int) {
   int value = 8;
@@ -117,4 +120,74 @@ Test(examples, using_restrict_to_optimize_add) {
   cr_assert_eq(arr1[0], 5.0);
   cr_assert_eq(arr1[1], 7.0);
   cr_assert_eq(arr1[2], 9.0);
+}
+
+typedef struct {
+  double *vectorA;
+  double *vectorB;
+  double sum;
+  int length;
+} VectorInfo;
+
+typedef struct {
+  VectorInfo *info;
+  int beginningIndex;
+} Product;
+
+pthread_mutex_t mutexSum;
+
+void *dotProduct(void *prod) {
+  Product *product = (Product*)prod;
+  VectorInfo *vectorInfo = product->info;
+  int beginningIndex = product->beginningIndex;
+  int endingIndex = beginningIndex + vectorInfo->length;
+  double total = 0.0;
+  
+  for (int i = beginningIndex; i < endingIndex; i++) {
+    total += vectorInfo->vectorA[i] * vectorInfo->vectorB[i];
+  }
+  
+  pthread_mutex_lock(&mutexSum);
+  vectorInfo->sum += total;
+  pthread_mutex_unlock(&mutexSum);
+  
+  return (void*)0;
+}
+
+#define NUM_THREADS 4
+
+Test(examples, using_pthreads_for_dot_product) {
+  VectorInfo vectorInfo;
+  double vectorA[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
+  double vectorB[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
+
+  vectorInfo.vectorA = vectorA;
+  vectorInfo.vectorB = vectorB;
+  vectorInfo.length = 4;
+  pthread_t threads[NUM_THREADS];
+  void *status;
+  pthread_attr_t attr;
+  pthread_mutex_init(&mutexSum, NULL);
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  
+  int returnValue;
+  int threadNumber;
+  
+  for (threadNumber = 0; threadNumber < NUM_THREADS; threadNumber++) {
+    Product *product = malloc(sizeof(Product));
+    product->beginningIndex = threadNumber * vectorInfo.length;
+    product->info = &vectorInfo;
+    returnValue = pthread_create(&threads[threadNumber], &attr, dotProduct, (void*)product);
+    if (returnValue) {
+      cr_expect_fail("pthread_create failed with error code %d", returnValue);
+    }
+  }
+  
+  pthread_attr_destroy(&attr);
+  for (threadNumber = 0; threadNumber < NUM_THREADS; threadNumber++) {
+    pthread_join(threads[threadNumber], &status);
+  }
+  pthread_mutex_destroy(&mutexSum);
+  cr_assert_eq(vectorInfo.sum, 1496.0);
 }
